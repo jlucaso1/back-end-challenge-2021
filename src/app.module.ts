e2a750a0aa8034493a4c1ce1bc0cd761988ffab5
João Lucas de Oliveira Lopes <jlucaso@hotmail.com>
2022-03-13 19:48:40 -0300

    Added alert feature with events

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArticleModule } from './article/article.module';
 import { CronModule } from './cron/cron.module';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    EventEmitterModule.forRoot(),
     ScheduleModule.forRoot(),
     ArticleModule,
     CronModule,
--- src/cron/cron.service.ts
+++ src/cron/cron.service.ts
@@ -1,4 +1,5 @@
 import { Inject, Injectable } from '@nestjs/common';
+import { EventEmitter2, OnEvent } from '@nestjs/event-emitter';
 import { Cron } from '@nestjs/schedule';
 import axios from 'axios';
 import { Article } from 'src/article/entities/article.entity';
@@ -15,19 +16,34 @@ export class CronService {
     private eventRepository: Repository<Event>,
     @Inject('LAUNCH_REPOSITORY')
     private launchRepository: Repository<Launch>,
+    private eventEmitter: EventEmitter2,
   ) {}
+
   @Cron('0 9 * * *')
   async getDataAndImportToDB() {
-    // delete all entries from the database
-    await this.eventRepository.query('DELETE FROM "event"');
-    await this.launchRepository.query('DELETE FROM "launch"');
-    await this.articleRepository.query('DELETE FROM "article"');
+    try {
+      // delete all entries from the database
+      await this.eventRepository.query('DELETE FROM "event"');
+      await this.launchRepository.query('DELETE FROM "launch"');
+      await this.articleRepository.query('DELETE FROM "article"');
 
-    // get all articles from the API
-    var articles = (
-      await axios.get('https://api.spaceflightnewsapi.net/v3/articles')
-    ).data as Article[];
-    // save all articles to the database
-    var savedArticles = await this.articleRepository.save(articles);
+      // get all articles from the API
+      var articles = (
+        await axios.get('https://api.spaceflightnewsapi.net/v3/articles')
+      ).data as Article[];
+      // save all articles to the database
+      var savedArticles = await this.articleRepository.save(articles);
+    } catch (_) {
+      this.eventEmitter.emit(
+        'error.syncronize-articles',
+        'Failed to syncronize the database with api.',
+      );
+    }
+  }
+
+  @OnEvent('error.syncronize-articles')
+  handleOrderCreatedEvent(payload: string) {
+    // send email, message or anything to report the error
+    console.log(payload);
   }
 }
